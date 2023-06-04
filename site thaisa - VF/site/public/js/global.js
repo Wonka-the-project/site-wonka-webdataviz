1018b66aab4c3954f1827eaba5e4c6b826110ac2
nobregathsa <thaisa.costa@sptech.school>
2023-06-04 12:59:16 -0300

    atualizações foto de perfil

--- site thaisa - VF/site/public/js/global.js
+++ site thaisa - VF/site/public/js/global.js
@@ -1,50 +1,57 @@
-// function usuariomudar() {
-//     if (sessionStorage.NOME_USUARIO != "") {
-//         div_logineusuario.innerHTML = `
-//         <div>
-//         <img src="../vetores/usuario_foto.png" width="40px">
-//     </div>
+function usuariomudar() {
+    if (sessionStorage.NOME_USUARIO != "") {
+        div_logineusuario.innerHTML = `
+        <div>
+        <img src="../vetores/usuario_foto.png" width="40px">
+    </div>
 
-//     <div style="margin-top: 15px;">
-//         <span style="color: white;">Olá,<span id="nome_usuario"></span></span>
-//         <div style="display: flex; align-items: center; margin-top: -20px; gap: 5px; width: 120px;">
-//             <img src="../vetores/estrela.png" width="20px">
-//             <h4 style="color: white;"><span>150</span> Pontos</h4>
-//         </div>
-//     </div>
+    <div style="margin-top: 15px;">
+        <span style="color: white;">Olá,<span id="nome_usuario"></span></span>
+        <div style="display: flex; align-items: center; margin-top: -20px; gap: 5px; width: 120px;">
+            <img src="../vetores/estrela.png" width="20px">
+            <h4 style="color: white;"><span id="ponto_usuario"></span> Pontos</h4>
+        </div>
+    </div>
 
-//     <div>
-//         <button onclick="loginmudar()" style="background-color: transparent; border: none; cursor: pointer;"><img src="../vetores/sair.png" width="20px"></button>
-//     </div>
-//         `
-//     cadastrar.style.display = 'none'
-//     }
+    <div>
+        <button onclick="loginmudar()" style="background-color: transparent; border: none; cursor: pointer;"><img src="../vetores/sair.png" width="20px"></button>
+    </div>
+        `
+    cadastrar.style.display = 'none'
+    }
 
-//     substituir()
-// }
+    substituir()
+}
 
-// var pontos_usuario = 0
+var pontos_usuario = 0
 
-// function substituir(){
-//     var nome_user = sessionStorage.getItem("NOME_USUARIO")
-//     nome_usuario.innerHTML = `${nome_user}`
-//     var idUsuario = sessionStorage.getItem("ID_USUARIO")
+function substituir(){
+    var nome_user = sessionStorage.getItem("NOME_USUARIO")
+    nome_usuario.innerHTML = `${nome_user}`
+    var idUsuario = sessionStorage.getItem("ID_USUARIO")
 
-//     fetch(`/usuarios/obterPontos/${idUsuario}`).then(function (resposta) {
-//         if (resposta.ok) {
-//             resposta.json().then(function (resposta) {
-//                 console.log("Dados recebidos: ", JSON.stringify(resposta));
-//                 pontos_usuario = resposta[0].contagem
+    fetch(`/usuarios/substituir/${idUsuario}`).then(function (resposta) {
+        if (resposta.ok) {
+            resposta.json().then(function (resposta) {
+                console.log("Dados recebidos: ", JSON.stringify(resposta));
+                pontos_usuario = resposta[0].pontos;
+    
+                ponto_usuario.innerHTML = `${resposta[0].pontos}`;
+    
+                console.log(pontos_usuario); // Aqui o valor de pontos_usuario estará correto
+            });
+        } else {
+            throw ('Houve um erro na API!');
+        }
+    }).catch(function (resposta) {
+        console.error(resposta);
+    });
 
-//                 id_1.innerHTML = `${contar}`
-//             });
-//         } else {
-//             throw ('Houve um erro na API!');
-//         }
-//     }).catch(function (resposta) {
-//         console.error(resposta);
-//     });
-//     console.log(contar)
+}
 
-// }
-
+function loginmudar(){
+        div_logineusuario.innerHTML = `
+        <a href="./login.html"><img src="../vetores/login.svg" width="120px" class="ticket_login"></a>
+        `
+        cadastrar.style.display = 'block'
+}
